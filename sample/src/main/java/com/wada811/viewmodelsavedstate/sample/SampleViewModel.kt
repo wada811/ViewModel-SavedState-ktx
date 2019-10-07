cb28e1c58840b067a9e6e7574f6c41725d751d6c
wada811 <at.wada811@gmail.com>
2019-10-07 22:11:04 +0900

    Support enum and custom delegate

--- sample/src/main/java/com/wada811/viewmodelsavedstate/sample/SampleViewModel.kt
+++ sample/src/main/java/com/wada811/viewmodelsavedstate/sample/SampleViewModel.kt
@@ -7,29 +7,35 @@ import androidx.lifecycle.SavedStateHandle
 import com.wada811.viewmodelsavedstate.SavedStateViewModel
 
 class SampleViewModel(savedStateHandle: SavedStateHandle) : SavedStateViewModel(savedStateHandle) {
+    enum class CountUpValue(val count: Int) {
+        ONE(1),
+        TEN(10)
+    }
+
     private var viewModelCount = MutableLiveData(0)
     var viewModelCountText: LiveData<String> = MediatorLiveData<String>().also { liveData ->
         liveData.addSource(viewModelCount) { count ->
             liveData.value = "$count"
         }
     }
-
-    val savedStateCount: MutableLiveData<Int> by savedStateLiveData(SampleViewModel::savedStateCount.name)
+    val countUpValueIntLiveData: MutableLiveData<Int?> by savedStateLiveData(this::countUpValueEnumLiveData.name)
+    val countUpValueEnumLiveData: MutableLiveData<CountUpValue?> by savedStateLiveData()
+    val savedStateCount: MutableLiveData<Int> by savedStateLiveData()
     var savedStateCountText: LiveData<String> = MediatorLiveData<String>().also { liveData ->
         liveData.addSource(savedStateCount) { count ->
             liveData.value = "$count"
         }
     }
 
-    val log: MutableLiveData<String> by savedStateLiveData(SampleViewModel::log.name)
+    val log: MutableLiveData<String> by savedStateLiveData()
 
     init {
         appendLog("ViewModel::init")
     }
 
     fun countUp() {
-        viewModelCount.value = viewModelCount.value?.plus(1)
-        savedStateCount.value = savedStateCount.value?.plus(1)
+        viewModelCount.value = viewModelCount.value?.plus(CountUpValue.values().firstOrNull { it.ordinal == countUpValueIntLiveData.value }?.count ?: 0)
+        savedStateCount.value = savedStateCount.value?.plus(countUpValueEnumLiveData.value?.count ?: 0)
     }
 
     fun appendLog(text: String) {
