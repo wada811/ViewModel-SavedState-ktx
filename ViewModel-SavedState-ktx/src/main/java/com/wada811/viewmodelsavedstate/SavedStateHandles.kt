0a45d552feedcbebb36e117ff9bc215718ee130c
wada811 <at.wada811@gmail.com>
2020-11-05 23:57:54 +0900

    Change to PropertyDelegateProvider

--- ViewModel-SavedState-ktx/src/main/java/com/wada811/viewmodelsavedstate/SavedStateHandles.kt
+++ ViewModel-SavedState-ktx/src/main/java/com/wada811/viewmodelsavedstate/SavedStateHandles.kt
@@ -3,54 +3,51 @@ package com.wada811.viewmodelsavedstate
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.wada811.viewmodelsavedstate.property.ReadOnlyPropertyProvider
-import com.wada811.viewmodelsavedstate.property.ReadWritePropertyProvider
 import com.wada811.viewmodelsavedstate.property.SavedStateLiveDataProperty
 import com.wada811.viewmodelsavedstate.property.SavedStateLiveDataSerializableProperty
 import com.wada811.viewmodelsavedstate.property.SavedStateProperty
 import com.wada811.viewmodelsavedstate.property.SavedStateSerializableProperty
-import com.wada811.viewmodelsavedstate.property.provideReadOnlyProperty
-import com.wada811.viewmodelsavedstate.property.provideReadWriteProperty
+import kotlin.properties.PropertyDelegateProvider
 import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 fun <T> SavedStateHandle.property(): ReadWriteProperty<ViewModel, T> = SavedStateProperty(this)
-fun <T> SavedStateHandle.property(defaultValue: T): ReadWritePropertyProvider<ViewModel, T> {
-    return provideReadWriteProperty {
-        if (!this.contains(it)) {
-            this[it] = defaultValue
+fun <T> SavedStateHandle.property(defaultValue: T): PropertyDelegateProvider<ViewModel, ReadWriteProperty<ViewModel, T>> {
+    return PropertyDelegateProvider { _: ViewModel, property: KProperty<*> ->
+        if (!this.contains(property.name)) {
+            this[property.name] = defaultValue
         }
         property()
     }
 }
 
 fun <TValue, TState> SavedStateHandle.property(adapter: SavedStateAdapter<TValue, TState>): ReadWriteProperty<ViewModel, TValue> = SavedStateSerializableProperty(this, adapter)
-fun <TValue, TState> SavedStateHandle.property(adapter: SavedStateAdapter<TValue, TState>, defaultValue: TValue): ReadWritePropertyProvider<ViewModel, TValue> {
-    return provideReadWriteProperty {
-        if (!this.contains(it)) {
-            this[it] = adapter.toSavedState(defaultValue)
+fun <TValue, TState> SavedStateHandle.property(adapter: SavedStateAdapter<TValue, TState>, defaultValue: TValue): PropertyDelegateProvider<ViewModel, ReadWriteProperty<ViewModel, TValue>> {
+    return PropertyDelegateProvider { _: ViewModel, property: KProperty<*> ->
+        if (!this.contains(property.name)) {
+            this[property.name] = adapter.toSavedState(defaultValue)
         }
         property(adapter)
     }
 }
 
 fun <T> SavedStateHandle.liveData(): ReadOnlyProperty<ViewModel, MutableLiveData<T>> = SavedStateLiveDataProperty(this)
-fun <T> SavedStateHandle.liveData(defaultValue: T): ReadOnlyPropertyProvider<ViewModel, MutableLiveData<T>> {
-    return provideReadOnlyProperty {
-        if (!this.contains(it)) {
-            this[it] = defaultValue
+fun <T> SavedStateHandle.liveData(defaultValue: T): PropertyDelegateProvider<ViewModel, ReadOnlyProperty<ViewModel, MutableLiveData<T>>> {
+    return PropertyDelegateProvider { _: ViewModel, property: KProperty<*> ->
+        if (!this.contains(property.name)) {
+            this[property.name] = defaultValue
         }
         liveData()
     }
 }
 
 fun <TValue, TState> SavedStateHandle.liveData(adapter: SavedStateAdapter<TValue, TState>): ReadOnlyProperty<ViewModel, MutableLiveData<TValue>> = SavedStateLiveDataSerializableProperty(this, adapter)
-fun <TValue, TState> SavedStateHandle.liveData(adapter: SavedStateAdapter<TValue, TState>, defaultValue: TValue): ReadOnlyPropertyProvider<ViewModel, MutableLiveData<TValue>> {
-    return provideReadOnlyProperty {
-        if (!this.contains(it)) {
-            this[it] = adapter.toSavedState(defaultValue)
+fun <TValue, TState> SavedStateHandle.liveData(adapter: SavedStateAdapter<TValue, TState>, defaultValue: TValue): PropertyDelegateProvider<ViewModel, ReadOnlyProperty<ViewModel, MutableLiveData<TValue>>> {
+    return PropertyDelegateProvider { _: ViewModel, property: KProperty<*> ->
+        if (!this.contains(property.name)) {
+            this[property.name] = adapter.toSavedState(defaultValue)
         }
         liveData(adapter)
     }
 }
-
